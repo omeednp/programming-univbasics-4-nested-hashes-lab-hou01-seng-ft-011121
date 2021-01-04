c0ae1600c66bb2c920d3e961d8bcaf73c1ee528d
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 10:00:05 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,6 +14,8 @@ def hopper
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -36,6 +37,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +58,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -81,7 +86,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  return programmer_hash
 end
 
 def changing_alan
@@ -104,6 +112,10 @@ def changing_alan
       }
     }
 
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  
+  return programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +137,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+  
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  return programmer_hash
 
 end
